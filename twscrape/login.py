3396af6bbebe71aafadc5e1835d82c4e0d184756
akjadoon <asfandyarkjadoon@gmail.com>
2025-04-21 21:59:39 +0500

    add new headers

--- twscrape/login.py
+++ twscrape/login.py
@@ -262,6 +262,9 @@ async def login(acc: Account, cfg: LoginConfig | None = None) -> Account:
         guest_token = await get_guest_token(client)
         client.headers["x-guest-token"] = guest_token
         client.headers["x-client-transaction-id"] = "eD0hCT0DlVck/2twHEDA7l98KItZFCoyyCupbVx6wj6VR9BhPZKCdkjwidakXFgDXLCGC3o4WNhc3Fws0iWAO2X7LZsUew"
+        client.headers["x-twitter-auth-type"] = "OAuth2Session"
+        # client.headers["X-Client-UUID"] = "648945bf-7ca2-4326-a4e9-cb4cbba2f99b"
+        
         rep = await login_initiate(client)
         ctx = TaskCtx(client, acc, cfg, None, imap)
         while True:
