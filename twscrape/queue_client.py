7200fbe1b6bb6f0225e398586673b324632f83c4
akjadoon <asfandyarkjadoon@gmail.com>
2025-04-23 16:45:53 +0500

    add header

--- twscrape/queue_client.py
+++ twscrape/queue_client.py
@@ -214,7 +214,8 @@ class QueueClient:
                  headers={
                 "x-client-transaction-id": "eD0hCT0DlVck/2twHEDA7l98KItZFCoyyCupbVx6wj6VR9BhPZKCdkjwidakXFgDXLCGC3o4WNhc3Fws0iWAO2X7LZsUew",
                 "x-twitter-auth-type":  "OAuth2Session",
-                "X-Client-UUID":  "648945bf-7ca2-4326-a4e9-cb4cbba2f99b"
+                "X-Client-UUID":  "648945bf-7ca2-4326-a4e9-cb4cbba2f99b",
+                "x-twitter-active-user": 'yes'
                 })
                 setattr(rep, "__username", ctx.acc.username)
                 await self._check_rep(rep)
