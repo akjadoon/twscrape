2fde4951c0c2e1582b536c376d199521cf338b3f
akjadoon <asfandyarkjadoon@gmail.com>
2025-04-23 17:55:42 +0500

    remove raise err

--- twscrape/queue_client.py
+++ twscrape/queue_client.py
@@ -200,7 +200,6 @@ class QueueClient:
 
     async def get(self, url: str, params: ReqParams = None) -> Response | None:
         logger.error("twscrape-log:", "req with headers",  self.ctx.clt.headers,  self.ctx.clt.headers)
-        raise HandledError()
         return await self.req("GET", url, params=params)
 
     async def req(self, method: str, url: str, params: ReqParams = None) -> Response | None:
